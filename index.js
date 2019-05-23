ce46857bfc64e55ed522642a1928f205e9b85d31
Ahmad yar meo <ahmadyarmeo@gmail.com>
2019-05-24 00:06:18 +0500

    not give any port

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const mongoose = require('mongoose');
 const stripe = require("stripe")("sk_test_ywGSmVRTxvcRc61SCNEpkqJ1007yKWAI6u");
 const FCM = require('fcm-node');
 
+app.set('port', (process.env.PORT || 5000));
+app.set('view engine', 'ejs');
+
 var serverKey = 'AIzaSyDuidqbHbrqmrjw7iJ-W6KI2_A04TqhSVE';
 var fcm = new FCM(serverKey);
 
@@ -432,6 +435,6 @@ const sendPushNotification = data => {
     // });
 }
 
-server.listen(3000, () => {
+server.listen(app.get('port'), () => {
     console.log('server is running on port 3000');
 });
